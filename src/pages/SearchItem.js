02894e275fc9c159fbe6288e1b43299a3aba1de4
Abigail Nielson <abbynielson@Abigails-MBP-5.home>
2024-04-19 12:16:09 -0700

    adjusting file structure

---
+++ src/pages/SearchItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SearchItem = () => {
+    return (
+    <>
+        <div className='search-input'>
+            <input type={"text"} placeholder="SearchItemn" />
+        </div>
+        <h1 className='h1'>Search Results</h1>
+        <div className="items">
+            <table>
+                <thead>
+                    <tr>
+                        <th className=''>Item Name</th>
+                        <th className=''>Price</th>
+                        <th className=''>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr></tr>
+                </tbody>
+            </table>
+        </div>
+    </>
+    )
+}
+
+export default SearchItem;
